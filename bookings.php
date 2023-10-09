c3626d66c7a6aa73df274ca8ede3e528a204ad72
sm1ncH <78959537+sm1ncH@users.noreply.github.com>
2023-10-09 20:50:31 +0200

    dsa

--- bookings.php
+++ bookings.php
@@ -1,8 +1,13 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="style/bookings.css">
 </head>
+<body>
+    <div id="row">
 <?php
 require_once 'baza.php';
 require_once 'cookie.php';
@@ -19,17 +24,23 @@ try {
         $stmt2 = $pdo->prepare("SELECT * FROM users WHERE id = ?");
         $stmt2->execute([$row['user_id']]);
         while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+
+
+            echo "<div id='booking'>";
             echo "<p>", htmlspecialchars($row2['name']), "</p>";
             echo "<p>", htmlspecialchars($row2['surname']), "</p>";
             // date
             echo "<p>", htmlspecialchars($row['dat_zac']), "</p>";
             echo "<p>", htmlspecialchars($row['dat_kon']), "</p>";
             // button to delete specific booking
-            echo "<p><a href='delete_booking.php?id=" . $row['id'] . "'>Delete</a></p>";
+            echo "<button><a href='delete_booking.php?id=" . $row['id'] . "'>Delete</a></button>";
+            echo "</div>";
         }
     }
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 ?>
+</div>
+</body>
 </html>
