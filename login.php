790f980c2236f4b890bbf1d22f00476105484774
sm1ncH <78959537+sm1ncH@users.noreply.github.com>
2023-10-09 22:29:54 +0200

    hsdoua

--- login.php
+++ login.php
@@ -1,4 +1,31 @@
 <?php
+// require_once 'baza.php';
+// require_once 'cookie.php';
+
+// $e = $_POST['email'];
+// $p = $_POST['password'];
+
+// try {
+//     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+//     $stmt->execute([$e, $p]);
+//     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+//     if ($row) {
+//         $_SESSION['id'] = $row['id'];
+//         $_SESSION['ime'] = $row['ime'];
+//         $_SESSION['priimek'] = $row['priimek'];
+//         $_SESSION['email'] = $row['email'];
+//         echo "login";
+//         header("Location: index.php");
+//     } else {
+//         echo "login ne obstaja";
+//         header("Location: login.php");
+//     }
+// } catch (PDOException $e) {
+//     echo "Error: " . $e->getMessage();
+// }
+?>
+<?php
 require_once 'baza.php';
 require_once 'cookie.php';
 
@@ -6,11 +33,13 @@ $e = $_POST['email'];
 $p = $_POST['password'];
 
 try {
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
-    $stmt->execute([$e, $p]);
+    // Select just the password from the database for the given email
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$e]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($row) {
+    if ($row && password_verify($p, $row['password'])) {
+        // Successful login, password matches hash
         $_SESSION['id'] = $row['id'];
         $_SESSION['ime'] = $row['ime'];
         $_SESSION['priimek'] = $row['priimek'];
@@ -18,6 +47,7 @@ try {
         echo "login";
         header("Location: index.php");
     } else {
+        // Invalid login credentials
         echo "login ne obstaja";
         header("Location: login.php");
     }
--- signup_insert.php
+++ signup_insert.php
@@ -1,4 +1,33 @@
 <?php
+// require_once 'baza.php';
+
+// $n = $_POST['name'];
+// $s = $_POST['surname'];
+// $e = $_POST['email'];
+// $p = $_POST['password'];
+// $k = $_POST['kraj_id'];
+// $ph = $_POST['phone'];
+
+// try {
+//     // Check if the user already exists
+//     $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+//     $stmt->execute([$e]);
+//     $rowCount = $stmt->rowCount();
+
+//     if ($rowCount == 1) {
+//         header("Location: login.html");
+//         exit; // Make sure you exit after a header redirect
+//     } else {
+//         $stmt = $pdo->prepare("INSERT INTO users (name, surname, email, password, kraj_id, phone) VALUES (?, ?, ?, ?, ?, ?)");
+//         $stmt->execute([$n, $s, $e, $p, $k, $ph]);
+//         header("Location: index.php");
+//         exit; // Make sure you exit after a header redirect
+//     }
+// } catch (PDOException $e) {
+//     echo "Error: " . $e->getMessage();
+// }
+?>
+<?php
 require_once 'baza.php';
 
 $n = $_POST['name'];
@@ -7,6 +36,9 @@ $e = $_POST['email'];
 $p = $_POST['password'];
 $k = $_POST['kraj_id'];
 $ph = $_POST['phone'];
+
+// Hash the password
+$hashedPassword = password_hash($p, PASSWORD_DEFAULT);
 
 try {
     // Check if the user already exists
@@ -18,8 +50,9 @@ try {
         header("Location: login.html");
         exit; // Make sure you exit after a header redirect
     } else {
+        // Insert hashed password instead of plain password
         $stmt = $pdo->prepare("INSERT INTO users (name, surname, email, password, kraj_id, phone) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$n, $s, $e, $p, $k, $ph]);
+        $stmt->execute([$n, $s, $e, $hashedPassword, $k, $ph]);
         header("Location: index.php");
         exit; // Make sure you exit after a header redirect
     }
@@ -27,3 +60,4 @@ try {
     echo "Error: " . $e->getMessage();
 }
 ?>
+
