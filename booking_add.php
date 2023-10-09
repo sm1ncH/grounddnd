9f540e91900bcd3d4541f2592de1b461b8882df5
sm1ncH <78959537+sm1ncH@users.noreply.github.com>
2023-10-09 22:19:52 +0200

    dsadad

--- booking_add.php
+++ booking_add.php
@@ -7,12 +7,16 @@ $date_end = $_POST['date_end'];
 $date_start = $_POST['date_start'];
 $property_id = $_SESSION['property_id'];
 $user_id = $_SESSION['id'];
-
-try {
+if ($date_end === "" || $date_start === "") {
+    header("Location: booking.php?id=" . $property_id);
+    exit();
+}
+else{try {
     $stmt = $pdo->prepare("INSERT INTO bookings (dat_zac, dat_kon, property_id, user_id) VALUES (?, ?, ?, ?)");
     $stmt->execute([$date_start, $date_end, $property_id, $user_id]);
     header("Location: index.php");
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
-}
+}}
+
 ?>
