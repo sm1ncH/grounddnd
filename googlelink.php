d0df6ae1348694ebb2edeb367c2868eda941b161
“safkom” <miha@safko.eu>
2023-10-12 08:55:16 +0200

    asdasd

--- googlelink.php
+++ googlelink.php
@@ -4,39 +4,8 @@ session_start();
 $email = $_POST['email'];
 $password = $_POST['geslo'];
 
-$sql = "SELECT * FROM users WHERE email = ?";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$email]); // Pass parameters as an array
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-if ($result !== false) {
-    $hash = $result['geslo'];
-    if (password_verify($password, $hash)) {
-        $sql = "UPDATE users SET google_id = ? WHERE id = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$_SESSION['google_id'], $result['id']]);
-
-        $_SESSION['id'] = $row['id'];
-        $_SESSION['ime'] = $row['ime'];
-        $_SESSION['priimek'] = $row['priimek'];
-        $_SESSION['email'] = $row['email'];
-        setcookie('prijava', "Prijava uspešna.");
-        setcookie('good', 1);
-        header('Location: index.php');
-        exit();
-    } 
-    else {
-        setcookie('prijava', "Napačno geslo.");
-        setcookie('error', 1);
-        header('Location: google_addmail.php');
-        exit();
-    }
-} else {
-    setcookie('prijava', "Uporabnik z tem mailom ne obstaja.");
-    setcookie('error', 1);
-    header('Location: google_addmail.php');
-    exit();
-}
+echo $email;
+echo $password;
 
 $pdo = null; // Close the connection
 ?>
