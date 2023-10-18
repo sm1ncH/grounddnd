920a64ad084a70e76d13829b5d29dfb26859c343
Miha Šafranko <miha.safranko@gmail.com>
2023-10-18 22:32:27 +0200

    Update alerts.php

--- alerts.php
+++ alerts.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Miha Šafranko"/>
-    <meta name="author" content="Miha Šafranko" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title>Game Upload</title>
     <style>
